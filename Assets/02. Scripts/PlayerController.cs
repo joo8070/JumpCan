18ea98bcf3a2f92af130f6dd5394438547f058f9
DESKTOP-UOD6SLT\TFX245S <joo8070@gmail.com>
2020-10-21 22:42:53 +0900

    점프캔 시작

---
+++ Assets/02. Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerAnim
+{
+    public AnimationClip idle;
+    public AnimationClip jump;
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] int jumpPower;
+    [SerializeField] PlayerAnim animClip;
+    Animation anim;
+
+    bool isJump;
+    Rigidbody rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        Jump();
+    }
+    
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //isJump = true;
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+
+            anim.clip = animClip.jump;
+            anim.Play();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //if(collision.transform.tag == "Block")
+        //{
+        //    if(isJump)
+        //    {
+        //        isJump = false;
+        //
+        //        anim.clip = animClip.idle;
+        //        anim.Play();
+        //    }
+        //}
+    }
+}
